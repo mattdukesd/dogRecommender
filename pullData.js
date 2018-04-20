db6b85dd6f9353a4e48269ca719e6df50d8d4da6
mattdukesd <31395843+mattdukesd@users.noreply.github.com>
2018-04-19 19:19:13 -0700

    Add files via upload

---
+++ pullData.js
@@ -0,0 +1,8 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( data);
+});
+//d3.json(link, function(data){
+//  recommended_age = data
+//console.log(data);
+})
